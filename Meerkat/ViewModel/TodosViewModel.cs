4f2fe7225106f81dbb95e55ac2ef33013d825d9f
oldtimerza <bradleymorrisza@gmail.com>
2020-06-20 00:34:05 +0200

    Remove unnecessary usings

--- Meerkat/ViewModel/TodosViewModel.cs
+++ Meerkat/ViewModel/TodosViewModel.cs
@@ -1,10 +1,6 @@
 ﻿using Meerkat.Model;
 using Ninject;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Meerkat.ViewModel
 {
