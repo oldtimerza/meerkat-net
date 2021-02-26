91180643cd5bcc11953adf441d38ed16175d874d
oldtimerza <bradleymorrisza@gmail.com>
2021-02-26 21:33:03 +0100

    Change application shortcuts to match VIM shortcuts without modifiers

--- Meerkat/Views/MainWindow.xaml.cs
+++ Meerkat/Views/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using NHotkey.Wpf;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
@@ -33,6 +34,33 @@ namespace Meerkat.Views
             if(textBox != null)
             {
                 textBox.Clear();
+            }
+        }
+
+        private void TodoMessageText_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            /*This is a hack and should be cleaned up with another a proper way to do it. 
+             * For now it works the way I want to the app to though so I'm leaving it this way*/
+            
+            var shortCutKeys = new List<Tuple<Key, string>> {
+                new Tuple<Key, string>( Key.I, "i" ),
+                new Tuple<Key, string>( Key.J, "j" ), 
+                new Tuple<Key, string>( Key.K, "k" ),
+                new Tuple<Key, string>( Key.D, "d" ) 
+            };
+            if(!Keyboard.IsKeyDown(Key.LeftShift) && !Keyboard.IsKeyDown(Key.RightShift))
+            {
+                foreach (var shortcutKey in shortCutKeys)
+                {
+                    if (e.Key == shortcutKey.Item1)
+                    {
+                        TextBox target = (TextBox)sender;
+                        target.RaiseEvent(new TextCompositionEventArgs(InputManager.Current.PrimaryKeyboardDevice, new TextComposition(InputManager.Current, target, shortcutKey.Item2))
+                        {
+                            RoutedEvent = TextCompositionManager.TextInputEvent
+                        });
+                    }
+                }
             }
         }
     }
